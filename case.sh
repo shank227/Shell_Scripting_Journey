65dcdcec4d9ff94a70fdab30d1f7e01b5698a685
shank227 <shashank.bakshi03@gmail.com>
2026-01-14 19:08:22 +0000

    some questions regarding cases & conditionals.. in its respective files

--- case.sh
+++ case.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-case ${1,,} in 
-    shashank | Administrator)
-        echo "Hello, you are the boss"
+# case ${1,,} in 
+#     shashank | Administrator)
+#         echo "Hello, you are the boss"
+#         ;;
+    
+#     help)
+#         echo "Just Login bruh!!"
+#         ;;
+    
+#     *)
+#         echo "idk who u are but u ain't the boss of me. Enter a valid username"
+# esac
+
+## Script that takes name as input and reacts differently using case
+
+echo "Enter the username: "
+read Name
+
+case "$Name" in  
+    shashank | administrator)
+        echo "Welcome Boss.. u are the best.. EVERYONE ELSE SUCKS"
         ;;
     
-    help)
-        echo "Just Login bruh!!"
+    bruce | darkknight)
+        echo "Hey welcome silent guardian.. go and greet ur master shashank"
+        ;;
+    
+    joker | harley)
+        echo "screw u guys.. get da hell out.. u are failures!!! USELESS"
         ;;
     
     *)
-        echo "idk who u are but u ain't the boss of me. Enter a valid username"
+        echo "Enter a name bruh???!!!"
 esac
--- conditionals.sh
+++ conditionals.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-if [ ${1,,} = shashank ]; then 
-    echo "Oh, you are my boss"
+# if [ ${1,,} = shashank ]; then 
+#     echo "Oh, you are my boss"
 
-elif [ ${1,,} = help ]; then
-    echo "Just login bruh" 
+# elif [ ${1,,} = help ]; then
+#     echo "Just login bruh" 
+
+# else 
+#     echo "idk who u are but u ain't the boss of me"
+
+# fi
+
+##Questions
+#1. Scripte checks if the file exits and prints a message
+
+echo "Enter the File Name: "
+read FileName
+
+if [[ -f "$FileName" ]]; then
+    echo "Yes $FileName exists"
 
 else 
-    echo "idk who u are but u ain't the boss of me"
-
+    echo "Nah bro it doesnn;t exist.. open up ur eyes"
 fi
