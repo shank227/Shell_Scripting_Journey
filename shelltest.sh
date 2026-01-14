b5986cc7bb0bc40c219c01f0abbfa31ee6c7395e
shank227 <shashank.bakshi03@gmail.com>
2026-01-14 08:13:21 +0000

    Shell_test_file_all_basic_concepts

---
+++ shelltest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Hello world
+# echo Hello World!
+
+## variables
+# FirstName=Shashank 
+# LastName=Bakshi
+# echo Hello $FirstName $LastName
+
+## Input/Output Redirection
+echo What is your First Name
+read FirstName
+echo What is your Last Name
+read LastName
+
+echo Hello $FirstName $LastName
